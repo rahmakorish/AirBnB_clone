b733f4a306f1d35b3f5c2f148d402af18296c46a
rahma korish <rahmakorish@gmail.com>
2024-02-12 08:52:12 -0800

    serilization

--- models/base_model.py
+++ models/base_model.py
@@ -23,6 +23,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """return class object"""
