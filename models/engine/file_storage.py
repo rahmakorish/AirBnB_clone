3c7166c4ab40b432275b03d88bc2eb5b9d83809b
ehab-al-Mawardi <ihabalmawardi@gmail.com>
2024-02-11 16:13:33 -0800

    Storage file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,6 +5,11 @@ import json
 import uuid
 import datetime
 from models.base_model import BaseModel
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
